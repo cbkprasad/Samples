831724c775d87036b07c7b6331bb1070916605a0
cbkprasad <cbkprasad@gmail.com>
2015-05-18 13:25:53 -0500

    First MVC Commit

---
+++ MvcFirstSample/Models/Orders.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcFirstSample.Models
+{
+    public class Orders
+    {
+        
+        public virtual List<Products> Product { get; set; }
+
+        [Key]
+        public int OrderID { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        [Display(Name="Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string CustomerEmail { get; set; }
+    }
+}
---
+++ MvcFirstSample/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcFirstSample.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+     }
+}
